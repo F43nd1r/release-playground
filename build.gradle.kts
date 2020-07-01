88ee84d9531bb937090342e23ff96aa10ec01e97
f43nd1r <lukas.morawietz@gmail.com>
2020-07-01 14:56:54 +0200

    initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,28 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    kotlin("jvm") version "1.3.72"
+    id("com.faendir.gradle.release") version "0.1"
+}
+dependencies {
+    implementation(kotlin("stdlib-jdk8"))
+}
+repositories {
+    mavenCentral()
+}
+val compileKotlin: KotlinCompile by tasks
+compileKotlin.kotlinOptions {
+    jvmTarget = "1.8"
+}
+val compileTestKotlin: KotlinCompile by tasks
+compileTestKotlin.kotlinOptions {
+    jvmTarget = "1.8"
+}
+
+tasks.register("fakePublish") {
+    doLast {
+        file("published.txt").writeText("Publish task was called")
+    }
+}
+
+tasks["afterReleaseBuild"].dependsOn(tasks["fakePublish"])
