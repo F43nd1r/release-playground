a7afdfccf7636c30932f83c0f4709368fd1eff61
f43nd1r <lukas.morawietz@gmail.com>
2020-07-08 00:33:05 +0200

    get new version test

--- build.gradle.kts
+++ build.gradle.kts
@@ -26,3 +26,11 @@ tasks.register("fakePublish") {
 }
 
 tasks["afterReleaseBuild"].dependsOn(tasks["fakePublish"])
+
+
+tasks.register("GetNewVersion") {
+    doLast {
+        println("New version is ${project.version}")
+    }
+}
+tasks["updateVersion"].finalizedBy("GetNewVersion")
