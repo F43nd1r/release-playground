2c1e1ab5ccc035b7598bdabf626b94b2a2d208f6
f43nd1r <lukas.morawietz@gmail.com>
2020-11-26 11:22:17 +0100

    add buildSrc and submodule

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     kotlin("jvm") version "1.3.72"
-    id("com.faendir.gradle.release") version "3.4.0-SNAPSHOT"
+    id("com.faendir.gradle.release") version "3.4.0"
 }
 dependencies {
     implementation(kotlin("stdlib-jdk8"))
